52e956948abddac9b02066ccb3f4612c2acc528b
Magen Crawford <crawford.magen@gmail.com>
2019-12-04 19:52:57 -0600

    finishing datatypes

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,36 +1,36 @@
 // display the current time and date
-var now = new Date();
-console.log("The time at the beep is", now);
+let now = new Date();
+console.log("The current date and time is ", now);
 
 // Convert a number to a string
-var num = 15;
-var a = num.toString(16);
-console.log(a);
+let num = 15;
+let abs = num.toString();
+console.log("The string representing the number is ", abs);
 
 // Convert a string to a number
-var x = "1000";
-var n = Number(x);
+let x = "1000";
+let n = Number(x);
 console.log(n);
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-var bookTitle = "The Shinning";
+let bookTitle = "The Shinning";
 console.log(typeof bookTitle);
 
 //prints out number
-var numbers = 100;
+let numbers = 100;
 console.log(typeof numbers);
 
 //prints out boolean
-var booleans = true;
+let booleans = true;
 console.log(typeof booleans);
 //prints out undefined
-var car;
+let car;
 console.log(typeof car);
 //prints out object instead of null because the datatype of null is object
-var nul = null;
+let nul = null;
 console.log(typeof nul);
 //prints out NaN, using type of prints out number
-var nun = NaN * 0;
+let nun = NaN * 0;
 console.log(nun);
 
 // Add two numbers together in javascript
@@ -39,30 +39,30 @@ let secondNum = 30;
 console.log(firstNum + secondNum);
 
 // Prints out "Both are TRUE" only when 2 things are true.
-var a = 1;
-var b = 2;
+let ac = 1;
+let bc = 2;
 function lessThan(a, b) {
-  if (a < 100 && b < 100) {
+  if (ac < 100 && bc < 100) {
     console.log("Both are True");
   }
 }
 console.log(lessThan(1, 2));
 
 // Prints out "One of these is TRUE" when 1 of 2 things are true.
-var a = 1;
-var b = 12;
+let ca = 1;
+let cb = 12;
 function moreThan(a, b) {
-  if (a < 10 || b < 10) {
+  if (ca < 10 || cb < 10) {
     console.log("One of these are True");
   }
 }
 console.log(moreThan(1, 12));
 
 // Prints out "Neither is TRUE" when both things are not true.
-var a = 11;
-var b = 12;
+let ar = 11;
+let br = 12;
 function notTrue(a, b) {
-  if (a < 10 && b < 10) {
+  if (ar < 10 && br < 10) {
     console.log("Both are true");
   } else {
     console.log("Neither is true");
